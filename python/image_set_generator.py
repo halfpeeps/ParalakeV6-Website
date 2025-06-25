26941d1dbff377c2b672fde1505b73646da9a175
peeps <101641076+halfpeeps@users.noreply.github.com>
2025-06-25 12:25:41 +0100

    Added V5 images

--- python/image_set_generator.py
+++ python/image_set_generator.py
@@ -3,9 +3,9 @@ import os
 import json
 
 # --- CONFIGURATION ---
-BASE_IMAGE_DIR = "images/07Classic-Paralake-Maps"  # relative to project root
+BASE_IMAGE_DIR = "images/16Classic-Paralake-Maps"  # relative to project root
 OUTPUT_JSON_DIR = "json/image_sets"  # where to write json files
-URL_PREFIX = "/images/07Classic-Paralake-Maps"  # web path to images
+URL_PREFIX = "/images/16Classic-Paralake-Maps"  # web path to images
 OUTPUT_JSON_NAME = "classic-screenshots.json"  # output file name
 
 IMAGE_EXTENSIONS = {".jpg", ".jpeg", ".png", ".webp", ".gif", ".bmp", ".tiff"}
