505aebe1f833960547e79f24fd672d2d17be2c43
peeps <101641076+halfpeeps@users.noreply.github.com>
2025-11-06 03:16:38 +0000

    Convert site to next.js

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    window.location.href = '/downloads.html';
+  }, []);
+
+  return <p>Redirecting...</p>;
+}
